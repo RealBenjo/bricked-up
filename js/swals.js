c9b2eef5899c07f01439e739400eaec4b4858c9c
Benjamin Ipavec <benjamin.ipavec@gmail.com>
2026-05-18 19:52:03 +0200

    credits + hidden credits in absolute top left of the screen

--- js/swals.js
+++ js/swals.js
@@ -190,3 +190,17 @@ function removeScores() {
     showLeaderboard(); 
   });
 }
+
+
+function showCredits() {
+  Swal.fire({
+    title: 'Credits',
+    html: 'This site is made by <br><strong>Benjamin Ipavec</strong>',
+    icon: 'info',
+    iconColor: '#d4af37',
+    confirmButtonText: 'Nice!',
+    confirmButtonColor: '#3085d6',
+    background: '#2f2f2f',
+    color: '#fff'
+  });
+}
